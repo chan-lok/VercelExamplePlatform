e85da2bac186989d0ef82c0f7eeb3f50c6555832
Lok Chan <lok@kosmonaut.io>
2023-06-06 16:18:32 +0200

    Override domain redirect in middleware settings

--- middleware.ts
+++ middleware.ts
@@ -18,15 +18,10 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
+  const hostname = req.headers.get("host") || "example-platform.vercel.app";
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
-
-  // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
@@ -35,8 +30,8 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.example-platform.vercel.app`, "")
+          .replace(`.platformize.vercel.app`, "") // no team on hobby account in vercel, so, no team slug (
       : hostname.replace(`.localhost:3000`, "");
 
   // rewrites for app pages
@@ -55,7 +50,10 @@ export default async function middleware(req: NextRequest) {
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
+  if (
+    hostname === "localhost:3000" ||
+    hostname === "example-platform.vercel.app"
+  ) {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
